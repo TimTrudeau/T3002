ae8944f504368ca19b166ad473641ed7b4e19c60
Tim Trudeau <timktrudeau@gmail.com>
2023-10-19 17:19:09 -0400

    Running on Pi. Need to test serial.

--- cyclebot.py
+++ cyclebot.py
@@ -3,11 +3,9 @@
 """
 
 import os
-# import pydevd_pycharm
-# pydevd_pycharm.settrace('localhost', port=22, stdoutToServer=True, stderrToServer=True)
-# if os.environ.get('DISPLAY','') == '':
-    # print('no display found. Using :0.0')
-    # os.environ.__setitem__('DISPLAY', ':0.0')
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
 
 import src.Page_GUI.pdrobot_support
 
